8fed1f0db2eacec2613d0d62d0788178509eccf8
Thomas Randall <36749093+tlranda@users.noreply.github.com>
2021-05-13 11:56:21 -0400

    Create vocab directory just in case

--- data/getText8.sh
+++ data/getText8.sh
@@ -7,6 +7,7 @@ echo "Fetch the text8 dataset"
 
 if [[ ! -d "corpus" ||  ! -e "corpus/text8" ]]; then
   mkdir -p corpus;
+  mkdir -p vocab;
   wget http://mattmahoney.net/dc/text8.zip -O corpus/text8.gz
   gzip -d corpus/text8.gz -f
 fi
