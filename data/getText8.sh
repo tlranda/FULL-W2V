2e304f9d630dbc5b67a6952d154ad729fc549c9a
Thomas Randall <36749093+tlranda@users.noreply.github.com>
2021-05-13 11:45:05 -0400

    Similar update for text8

--- data/getText8.sh
+++ data/getText8.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Anchor path to script directory                                              
+anchor=$(dirname $(realpath $0));                                              
+cd $anchor;      
 echo "Fetch the text8 dataset"
 
-if [ ! -e text8 ]; then
-  wget http://mattmahoney.net/dc/text8.zip -O text8.gz
-  gzip -d text8.gz -f
+if [[ ! -d "corpus" ||  ! -e "corpus/text8" ]]; then
+  mkdir -p corpus;
+  wget http://mattmahoney.net/dc/text8.zip -O corpus/text8.gz
+  gzip -d corpus/text8.gz -f
 fi
